e7d7f601ada2272ebb2b9617d0dbdf32061e1429
HNNX <nejc.stibilj01@gmail.com>
2020-02-10 18:54:08 +0100

    jQuery

---
+++ mojScript.js
@@ -0,0 +1,11 @@
+function izpisi() {
+    document.getElementById("mojDiv").innerHTML = "Danes je lep dan";
+}
+
+function prikaziElement() {
+    document.getElementById("drugiDiv").style.visibility = "visible";
+}
+
+function skrijElement() {
+    document.getElementById("drugiDiv").style.visibility = "hidden";
+}
